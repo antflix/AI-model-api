09785c9f898d9fadcf3f6af9f94c7a82dbeeaaff
Anthony <anthonymeo88@gmail.com>
2024-04-09 21:01:24 -0500

    Initial Commit

---
+++ api/apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiApp.swift
+//  api
+//
+//  Created by Anthony on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
